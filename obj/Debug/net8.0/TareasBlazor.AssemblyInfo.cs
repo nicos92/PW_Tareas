091a63de3f6a0682e9004ed800f58bdce545ab2a
nicosw11 <nicolassandoval140692@gmail.com>
2026-05-04 17:38:51 -0300

    agrega componente de tarjeta para las tareas

--- obj/Debug/net8.0/TareasBlazor.AssemblyInfo.cs
+++ obj/Debug/net8.0/TareasBlazor.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TareasBlazor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2fd0d68ac3947c92f2af6787e98d3e652be31b5a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3bd2744c666a6392e7b8115ef37cd0c51155288c")]
 [assembly: System.Reflection.AssemblyProductAttribute("TareasBlazor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TareasBlazor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
